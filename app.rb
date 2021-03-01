426b26fa30413fdc37db0b98cde83ca039774e6f
Jon Stump <jmstump@gmail.com>
2021-03-01 14:54:52 -0800

    add ability to add and view albums
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -3,3 +3,26 @@ require('sinatra/reloader')
 require('./lib/album')
 require('pry')
 also_reload('lib/**/*.rb')
+
+
+get('/') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums/new') do
+  erb(:new_album)
+end
+
+post('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all()
+  erb(:albums)
+end
